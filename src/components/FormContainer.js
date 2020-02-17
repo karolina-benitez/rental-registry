1d33615e3cb58ac0ec8deeb52a95d02278d949c2
Philliph Drummond <oilsinwater@gmail.com>
2020-02-16 18:24:10 -0800

    feat: adds Form with name inputs

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import { Form } from 'react-bootstrap';
 
 
 class FormContainer extends Component {
@@ -12,7 +13,17 @@ class FormContainer extends Component {
         <div className="homepage">
           <h1 className="homepage-title">Submit New Rental Registry Form</h1>
           <hr />
-        
+          <Form>
+            <Form.Group controlId='ownerName'>
+              <Form.Text className='text-muted'> Owner </Form.Text>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control placeholder='first name' type="textarea" />
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control placeholder='last name' type="textarea" />
+            </Form.Group>
+          </Form>
+
+
         </div>
         <Footer />
       </React.Fragment>
