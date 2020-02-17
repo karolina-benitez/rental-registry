08af9f63ead0748b34493030dc4b5dd1707991a5
Philliph Drummond <oilsinwater@gmail.com>
2020-02-16 17:11:50 -0800

    format: changes About and FormContainer to functional components

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
-
-export class About extends Component {
-  render() {
-    return (
-      <div class="wrapper">
-        <Header />
-        <div className="homepage">
-          <h1 className="homepage-title">About</h1>
-          <hr />
-          <p className="homepage-body">
-            <span>Blurb</span>
-          </p>
-        </div>
-        <Footer />
-      </div>
-    )
-  }
+const About = () => {
+  return <div class="wrapper">
+    <Header />
+    <div className="homepage">
+      <h1 className="homepage-title">About</h1>
+      <hr />
+      <p className="homepage-body">
+        <span>Blurb</span>
+      </p>
+    </div>
+    <Footer />
+  </div>
 }
-
 export default About
--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 
-export class FormContainer extends Component {
-  render() {
-    return (
-      <div class="wrapper">
-        <Header />
-        <div className="homepage">
-          <h1 className="homepage-title">Submit New Rental Registry Form</h1>
-          <hr />
-          <p className="homepage-body">
-            <span>Welcome!</span>
-          </p>
-        </div>
-        <Footer />
-      </div>
-    )
-  }
-}
+const FormContainer = () => {
+  return <div class="wrapper">
+    <Header />
+    <div className="homepage">
+      <h1 className="homepage-title">Submit New Rental Registry Form</h1>
+      <hr />
+      <p className="homepage-body">
+        <span>Welcome!</span>
+      </p>
+    </div>
+    <Footer />
+  </div>;
+};
 
 export default FormContainer;
