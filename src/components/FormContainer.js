4a34581708388412d467c30f6848d10997bf29ae
Philliph Drummond <oilsinwater@gmail.com>
2020-02-17 10:36:40 -0800

    feat: adds nextStep state

--- src/components/FormContainer.js
+++ src/components/FormContainer.js
@@ -1,34 +1,136 @@
+/* eslint-disable default-case */
 import React, { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import { Form } from 'react-bootstrap';
+import PropertyDetails from './PropertyDetails';
+import UnitDetails from './UnitDetails'
+import Thanks from './Thanks';
 
+export class FormContainer extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    ownership: '',
+    acquired: '',
+    street: '',
+    city: '',
+    state: '',
+    zipcode: '',
+    units: [
+      {
+      id: Number,
+      bedrooms: Number,
+      bathrooms: Number,
+      occupancyStatus: '',
+      monthOccupied: '',
+      yearOccupied: '',
+      monthsRented: Number,
+      vacancyDays: Number,
+      previousJanRent: '',
+      currentJanRent: '',
+      collectedJanUtil: '',
+      monthRentChanged: '',
+      dateRentChanged: '',
+      evictions: Number,
+      evictionReasons: '',
+      }
+    ],
+  }
+  
+  adduser = (unit) => {
+    unit.id = this.state.units.length + 1
+    this.setState([...this.state.units, unit])
+  }
 
-class FormContainer extends Component {
+  nextStep = () => {
+    const { step } = this.state
+    this.setState({
+      step: step + 1
+    })
+  }
+
+  prevStep = () => {
+    const { step } = this.state
+    this.setState({
+      step: step - 1
+    })
+  }
+
+  handleChange = input => event => {
+    this.setState({ [input]: event.target.value })
+  }
+
 
   render() {
-    return (
-      <React.Fragment>
-        <Header />
-        <div className="homepage">
-          <h1 className="homepage-title">Submit New Rental Registry Form</h1>
-          <hr />
-          <Form>
-            <Form.Group controlId='ownerName'>
-              <Form.Text className='text-muted'> Owner </Form.Text>
-              <Form.Label>First Name</Form.Label>
-              <Form.Control placeholder='first name' type="textarea" />
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control placeholder='last name' type="textarea" />
-            </Form.Group>
-          </Form>
+    const { step } = this.state;
+    const {
+      firstName,
+      lastName,
+      ownership,
+      acquired,
+      street,
+      city,
+      state,
+      zipcode,
+      units,
+      bedrooms,
+      bathrooms,
+      occupancyStatus,
+      monthOccupied,
+      yearOccupied,
+      monthsRented,
+      vacancyDays,
+      previousJanRent,
+      currentJanRent,
+      collectedJanUtil,
+      monthRentChanged,
+      dateRentChanged,
+      evictions,
+      evictionReasons
+    } = this.state;
 
+    const values = {
+      firstName,
+      lastName,
+      ownership,
+      acquired,
+      street,
+      city,
+      state,
+      zipcode,
+      units,
+      bedrooms,
+      bathrooms,
+      occupancyStatus,
+      monthOccupied,
+      yearOccupied,
+      monthsRented,
+      vacancyDays,
+      previousJanRent,
+      currentJanRent,
+      collectedJanUtil,
+      monthRentChanged,
+      dateRentChanged,
+      evictions,
+      evictionReasons
+    }
 
-        </div>
-        <Footer />
-      </React.Fragment>
-    )
+    switch (step) {
+      case 1:
+        return <PropertyDetails
+          nextStep={this.nextStep}
+          handleChange={this.handleChange}
+          values={values}
+        />
+      case 2:
+        return <UnitDetails
+          nextStep={this.nextStep}
+          handleChange={this.handleChange}
+          values={values}
+        />
+      case 3:
+        return <Thanks />
+    }
   }
 }
 
-export default FormContainer
+export default FormContainer;
---
+++ src/components/UnitDetails.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { Form, Button } from 'react-bootstrap';
+import Header from './Header';
+import Footer from './Footer';
+
+
+export class UnitDetails extends Component {
+  saveAndContinue = (e) => {
+    e.preventDefault()
+    this.props.nextStep()
+  }
+
+  render() {
+    const { values } = this.props;
+    return (
+      <React.Fragment>
+        <Header />
+
+        <Form>
+          <h1>Owner</h1>
+          <Form.Control as="textarea" placeholder='First name' onChange={this.props.handleChange('firstName')}
+            defaultValue={values.firstName} />
+          <Form.Label>First name</Form.Label>
+          <Button className="submit-new-form" onClick={this.saveAndContinue}>
+            <span>Submit</span>
+          </Button>
+        </Form>
+        <br></br>
+        <Footer />
+      </React.Fragment>
+    )
+  }
+}
+
+export default UnitDetails
